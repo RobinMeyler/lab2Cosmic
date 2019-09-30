bc09f4ff62c2d7784d9236fcbb6d3d1e3baa9caf
JoshTyrrellBrowne <c00224660@itcarlow.com>
2019-09-30 15:19:14 +0100

    NPC Behaviour started

---
+++ lab2Cosmic/Assets/Scripts/NPCBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehaviour : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        moveDirection = new Vector3( Random.Range(-10,10), Random.Range(-10, 10), Random.Range(-10, 10));
+        moveDirection.Normalize();
+    }
+
+    
+    void FixedUpdate()
+    {
+        rb2d.AddForce(moveDirection);
+        transform.Rotate(new Vector3(0, 0, Random.Range(10,60)) * Time.deltaTime);
+    }
+}
