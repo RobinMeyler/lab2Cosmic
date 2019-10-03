bcdc7f5d2b4b930cd17769ac20bdf6cf307ada50
JoshTyrrellBrowne <c00224660@itcarlow.com>
2019-10-03 18:42:25 +0100

    scoring/ mass and size stuff added

--- lab2Cosmic/Assets/Scripts/NPCBehaviour.cs
+++ lab2Cosmic/Assets/Scripts/NPCBehaviour.cs
@@ -6,7 +6,8 @@ public class NPCBehaviour : MonoBehaviour
 {
     Rigidbody2D rb2d;
     Vector3 moveDirection;
-    public float mass;
+    //public float mass;
+    float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -14,13 +15,23 @@ public class NPCBehaviour : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         moveDirection = new Vector3( Random.Range(-10,10), Random.Range(-10, 10), Random.Range(-10, 10));
         moveDirection.Normalize();
-        mass = Random.Range(10, 50);
+        //mass = Random.Range(10, 50);
+        speed = Random.Range(1, 4);
+
+        rb2d.mass = Random.Range(10, 100);
+
+        rb2d.transform.localScale = new Vector3(rb2d.mass / 50, rb2d.mass / 50, rb2d.mass / 50);
+
+        //change collider size to match new sprite size..
+        this.GetComponent<CircleCollider2D>().radius = this.GetComponent<SpriteRenderer>().bounds.size.magnitude / 2;
+
     }
 
     
     void FixedUpdate()
     {
-        rb2d.AddForce(moveDirection);
+        //rb2d.AddForce(moveDirection);
+        rb2d.velocity = (moveDirection * speed);
         transform.Rotate(new Vector3(0, 0, Random.Range(10,60)) * Time.deltaTime);
     }
 }
--- lab2Cosmic/Assets/Scripts/PlayerController.cs
+++ lab2Cosmic/Assets/Scripts/PlayerController.cs
@@ -12,14 +12,13 @@ public class PlayerController : MonoBehaviour
     float enemyMass;
     public float speed;
 
-    float massToScaleRatio; // -josh experiment..
-
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
 
-        massToScaleRatio = rb2d.transform.localScale.x / rb2d.mass;  // -josh experiment..
+        rb2d.mass = 50;
+        rb2d.transform.localScale = new Vector3(rb2d.mass / 50, rb2d.mass / 50, rb2d.mass / 50);
     }
 
     // Update is called once per frame
@@ -28,7 +27,27 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
+        // rb2d.AddForce(movement * speed);
+
+        // fast movement, JUST 4 TESTIN!
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb2d.transform.Translate(new Vector2(0, -0.1f));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb2d.transform.Translate(new Vector2(0, 0.1f));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.transform.Translate(new Vector2(-0.1f, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.transform.Translate(new Vector2(0.1f, 0));
+        }
+
 
         GameObject[] gos;
         gos = GameObject.FindGameObjectsWithTag("NPC");
@@ -44,7 +63,8 @@ public class PlayerController : MonoBehaviour
             }
         }
         // Mass to distance check here to enforce gravity
-        enemyMass = gos[index].GetComponent<NPCBehaviour>().mass;
+        //enemyMass = gos[index].GetComponent<NPCBehaviour>().mass;
+        enemyMass = gos[index].GetComponent<Rigidbody2D>().mass; // change by josh
         Vector3 gravforce = (gos[index].GetComponent<NPCBehaviour>().transform.position - transform.position).normalized;
 
         float gravDist = Vector3.Distance(transform.position, gos[index].transform.position);
@@ -58,13 +78,17 @@ public class PlayerController : MonoBehaviour
 
         if (collision.CompareTag("NPC"))
         {
-            rb2d.mass += collision.GetComponent<Rigidbody2D>().mass;  // add npc mass to player mass
-            rb2d.transform.localScale += collision.transform.localScale; // add npc scale to player scale
+            if (collision.GetComponent<Rigidbody2D>().mass < rb2d.mass)
+            {
+                rb2d.mass += collision.GetComponent<Rigidbody2D>().mass;  // add npc mass to player mass
+                rb2d.transform.localScale += collision.transform.localScale; // add npc scale to player scale
 
-            collision.gameObject.SetActive(false);
+                collision.gameObject.SetActive(false);
+                ScoreTextBehaviour.scoreCount += 1;
+            }
 
-           // transform.localScale = Vector3(1.0f, 1.0f, 1.0f);
-           //  mass += 10;
+            // transform.localScale = Vector3(1.0f, 1.0f, 1.0f);
+            //  mass += 10;
 
             score++;
 
